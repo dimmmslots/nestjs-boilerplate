4e393cf061e0fb801cd713f36140e0f1400d62ba
dimmmslots <vctrline2048@gmail.com>
2023-07-19 23:07:02 +0700

    fix: csurf init error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
 import { ConfigModule } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
 import { ZodValidationPipe } from 'nestjs-zod';
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { PORT } from './utils/constants';
+import { AppModule } from '@/app.module';
+import { PORT } from '@common/constants';
 import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import * as csurf from 'csurf';
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.use(cookieParser());
-  app.use(csurf());
+  app.use(csurf({ cookie: true }));
   app.setGlobalPrefix('/api');
   const port = PORT || 3000;
   await app.listen(port);
